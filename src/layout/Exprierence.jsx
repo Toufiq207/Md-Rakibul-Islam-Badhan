a9579802ae7d3981bcfe07b16782fc6928b223f0
ToufiqLizon <lijonlijon30@gmail.com>
2026-08-30 01:03:52 +0600

    update

---
+++ src/layout/Exprierence.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Exprierence = () => {
+  return (
+    <div>Exprierence</div>
+  )
+}
+
+export default Exprierence
--- src/page/Home.jsx
+++ src/page/Home.jsx
@@ -3,13 +3,19 @@ import Hero from '../component/Hero'
 import About from '../layout/About'
 import Service from '../layout/Service'
 import Contact from '../layout/Contact'
+import Project from '../layout/Project'
+import Skills from '../layout/Skills'
+import Exprierence from '../layout/Exprierence'
 
 const Home = () => {
   return (
     <div>
         <Hero/>
         <About/>
+        <Skills/>
+        <Exprierence/>
         <Service/>
+      <Project/>
         <Contact/>
     </div>
   )
